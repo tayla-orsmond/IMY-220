9bd119b80c90d549cbe3572defd9d3d522caa4c4
taylawantsasnack <tayla.orsmond@gmail.com>
2022-08-17 02:13:32 +0200

    Fixed errors and implemented upload system

--- Assignments/A2/login.php
+++ Assignments/A2/login.php
@@ -51,8 +51,8 @@
 
 				echo 	"<form action='login.php' method='POST' enctype='multipart/form-data'>
 							<div class='form-group'>
-								<input type='hidden' name='em' value='$email'/>
-								<input type='hidden' name='pa' value='$pass'/>
+								<input type='hidden' name='email' value='$email'/>
+								<input type='hidden' name='pass' value='$pass'/>
 								<label for='eventName'>Event Name:</label><br>
 								<input type='text' class='form-control' name='eventName' /><br>								
 								<label for='eventDescription'>Event Description:</label><br>
@@ -81,7 +81,7 @@
 			*/
 			if(isset($_POST["submit"]) && $email != null){
 				//If user submitted form and email is != null (i.e., logged in)
-				if(isset($_FILES["picToUpload[]"]) 
+				if(isset($_FILES["picToUpload"]) 
 				&& isset($_POST["eventName"])
 				&& isset($_POST["eventDescription"])
 				&& isset($_POST["eventDate"])){
@@ -89,91 +89,105 @@
 					/* 
 					* Get variables from form
 					*/
-					$images = $_FILES["picToUpload[]"];
-					$e_name = $_POST["eventName"];
-					$e_desc = $_POST["eventDescription"];
+					$images = $_FILES["picToUpload"];
+					$e_name = addslashes($_POST["eventName"]);
+					$e_desc = addslashes($_POST["eventDescription"]);
 					$e_date = $_POST["eventDate"];
-					$u_email = $_POST["em"];
-					$u_pass = $_POST["pa"];
+					$u_email = $_POST["email"];
+					$u_pass = $_POST["pass"];
 
 					/* 
 					* Check and Upload Image
 					*/
 					//check if the image is a jpg and smaller than 1MB
-					if($images["type"] != "image/jpg"
-					&& $images["type"] != "image/jpeg"
-					&& $images["type"] != "image/pjpeg"){
+					if($images["type"][0] != "image/jpg"
+					&& $images["type"][0] != "image/jpeg"
+					&& $images["type"][0] != "image/pjpeg"){
 						//File is not valid
-						echo "<p>File Type unsupported.</p>";
+						echo '<div class="alert alert-warning mt-3" role="alert">
+								File Type not supported.
+							</div>';
 					}
-					else if ($images["size"] > 1000000){
+					else if ($images["size"][0] > 1000000){
 						//File is larger than 1MB
-						echo "<p>File size is too large.</p>";
+						echo '<div class="alert alert-warning mt-3" role="alert">
+								File is too large.
+							</div>';
 					}
-					if($images["error"] > 0){
-						echo "<p>Error: " . $uploadFile["error"] . "</p><br/>";
+					else if($images["error"][0] > 0){
+						echo '<div class="alert alert-warning mt-3" role="alert">
+								Error: ' . $uploadFile["error"] . '
+							</div><br/>';
 					} 
-					if(($images["type"] == "image/jpg"
-					|| $images["type"] == "image/jpeg"
-					|| $images["type"] == "image/pjpeg")
-					&& $images["size"] < 1000000){
-						move_uploaded_file($images["tmp_name"], "gallery/" . $images["name"]);
-					}
-					/* 
-					* Make DB query
-					*/
-					//Get user data
-					$q = "SELECT user_id FROM tbusers WHERE email = '$u_email' AND password = '$u_pass'";
-					$r = $mysqli->query($q);
-					if(mysqli_num_rows($r) > 0){//user exists
-						$row = $r->fetch_assoc();
-						$u_id = $row['user_id'];
-						//Upload into events
-						$q2 = "INSERT INTO tbevents(user_id, name, description, date) VALUES ('$u_id', '$e_name', '$e_desc', '$e_date')";
-						if($mysqli->query($q2)){
-							//Get the event id of the newly uploaded event
-							$q3 = "SELECT event_id FROM tbevents WHERE user_id = '$u_id' AND name = '$e_name' AND date = '$e_date'";
-							$r = $mysqli->query($q3);
-							if(mysqli_num_rows($r) > 0){
-								$row = $r->fetch_assoc();
-								$e_id = $row['event_id'];
-								$n = $images["name"];
-								//Finally, upload image to gallery
-								$q4 = "INSERT INTO tbgallery(event_id, image_name) VALUES ('$e_id', '$n')";
-								if($mysqli->query($q4)){
-									echo "<p>Event successfully added.</p>";
+					else if(($images["type"][0] == "image/jpg"
+						|| $images["type"][0] == "image/jpeg"
+						|| $images["type"][0] == "image/pjpeg")
+						&& $images["size"][0] < 1000000){
+						
+						move_uploaded_file($images["tmp_name"][0], "gallery/" . $images["name"][0]);
+						/* 
+						* Make DB query
+						*/
+						//Get user data
+						$q = "SELECT user_id FROM tbusers WHERE email = '$u_email' AND password = '$u_pass'";
+						$r = $mysqli->query($q);
+						if(mysqli_num_rows($r) > 0){//user exists
+							$row = $r->fetch_assoc();
+							$u_id = $row["user_id"];
+							//Upload into events
+							$q2 = "INSERT INTO tbevents(user_id, name, description, date) VALUES ('$u_id', '$e_name', '$e_desc', '$e_date')";
+							if($mysqli->query($q2)){
+								//Get the event id of the newly uploaded event
+								$q3 = "SELECT event_id FROM tbevents WHERE user_id = '$u_id' AND name = '$e_name' AND date = '$e_date'";
+								$r = $mysqli->query($q3);
+								if(mysqli_num_rows($r) > 0){
+									$row = $r->fetch_assoc();
+									$e_id = $row["event_id"];
+									$n = $images["name"][0];
+									//Finally, upload image to gallery
+									$q4 = "INSERT INTO tbgallery(event_id, image_name) VALUES ('$e_id', '$n')";
+									if($mysqli->query($q4)){
+										echo '<div class="alert alert-success mt-3" role="alert">
+												Event uploaded sucessfully
+											</div>';
+									}
 								}
 							}
 						}
 					}
 				}
 				else{
-					echo "<p>Please ensure all form fields are filled in and valid before submitting.</p>";
+					echo '<div class="alert alert-warning mt-3" role="alert">
+							Please ensure all form fields are filled in and valid before submitting.
+						</div>';
 				}
 			}
 			/* 
 			* Display User events
 			*/
-			$u_id = ($res->fetch_assoc())["user_id"];
+			$query = "SELECT * FROM tbusers WHERE email = '$email' AND password = '$pass'";
+			$res = $mysqli->query($query);
+			$r = $res->fetch_assoc();
+			$uid = $r["user_id"][0];
 			echo "<h2>Events:</h2>
-					<div class='row eventsGallery'>";
+					<div class='row row-cols-4 eventsGallery'>";
 					//Get all events for a user
-				$event_query = "SELECT * FROM  tbevents WHERE user_id = '$u_id' ORDER BY date desc";
+				$event_query = "SELECT * FROM  tbevents WHERE user_id = '$uid' ORDER BY date DESC";
 				$events = $mysqli->query($event_query);
 				if(mysqli_num_rows($events) > 0){//Events exist
 					while($row = $events->fetch_assoc()){
-						$e_id = $row['event_id'];
-						$image_query = "SELECT name FROM tbgallery WHERE event_id = '$e_id'";
+						$eid = $row['event_id'];
+						$image_query = "SELECT image_name FROM tbgallery WHERE event_id = '$eid'";
 						$images = $mysqli->query($image_query);
-						$img = $image->fetch_assoc();
+						$img = $images->fetch_assoc();
 
 						echo "<div class='col'>
 							<div class='card'>
-								<div class='card-header'>" . $row["date"] ."</div>
+								<div class='h4 ms-2'>" . $row["date"] ."</div>
 								<div class='card-body'>
-									<img class='card-img-top' src='gallery/".$img["name"]."' alt='' />
-									<div class='card-title>" . $row["name"] ."</div>
-									<div class='card-text'>" . $row["description"] ."</div>
+									<img class='card-img-top' src='gallery/".$img["image_name"]."' alt='' />
+									<div class='card-title mt-2'>" . stripslashes($row["name"]) ."</div>
+									<div class='card-text'>" . stripslashes($row["description"]) ."</div>
 								</div>
 							</div>
 						</div>";

0c2d1bd93c3e96915042c2039e3a53b70ef012a5
taylawantsasnack <tayla.orsmond@gmail.com>
2022-08-19 08:28:42 +0200

    Completed tasks 1 and 2

--- Pracs/P3/script.js
+++ Pracs/P3/script.js
@@ -1,4 +1,8 @@
 //Tayla Orsmond u21467456
+/*
+ * Factorial Checker
+ *
+ */
 function FactorialChecker(){
     this.printFactorial = function(n){
         var factorial = 1;
@@ -14,12 +18,13 @@ function FactorialChecker(){
             if(fact % i === 0){
                 fact = parseInt(fact/i);
                 arr.push(i);
+                console.log(i);
             }
             else{
                 break;
             }
         }
-        if(i === 1){
+        if(fact === 1){
             return arr;
         }
         else{
@@ -46,14 +51,48 @@ const checker = new FactorialChecker();
 /* 
  * EVENT LISTENERS
  */
-GenerateFactorial.onclick(function(e){
+GenerateFactorial.addEventListener("click", function(e){
     const n = FactorialValue.value;
     var res = checker.printFactorial(n);
-    FactorialResult.innerHTML(res);
+    FactorialResult.innerHTML = res;
 });
 
-GenerateFactorialList.onclick(function(e){
+GenerateFactorialList.addEventListener("click", function(e){
     const fact = FactorialInput.value;
     var res = checker.printArray(fact);
-    FactorialListResult.innerHTML(res);
+    FactorialListResult.innerHTML = res;
+})
+
+/*
+ * Pig Latin
+ *
+ */
+const vowels = /(A|a|E|e|I|i|O|o|U|u)/;
+function PigLatinEncrypt(input){
+    //var arr = [...input];
+    if(input.search(vowels) === -1){
+        input += "ay";
+    }
+    else if(input.search(vowels) === 0){
+        input += "way";
+    }
+    else{
+        var FL = input.substring(0, 1);
+        input = input.slice(1);
+        input += FL + "ay";
+    }
+    return input;
+}
+/* 
+ * VARS
+ */
+//PigLatinEncryption
+const SentenceToConvert = document.getElementById("SentenceToConvert");
+const PigLatinEncryptBTN = document.getElementById("PigLatinEncrypt");
+var PigLatinResult = document.getElementById("PigLatinResult");
+
+PigLatinEncryptBTN.addEventListener("click", function(){
+    var input = SentenceToConvert.value;
+    var res = PigLatinEncrypt(input);
+    PigLatinResult.innerHTML = res;
 })

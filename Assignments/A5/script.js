13a3d52844b7af6e2e520d3e466ccf511a62b20e
taylawantsasnack <tayla.orsmond@gmail.com>
2022-09-08 18:49:39 +0200

    Completed Task 3 and bonus

--- Assignments/A5/script.js
+++ Assignments/A5/script.js
@@ -19,19 +19,18 @@ $(() => {
                 <b>${date}</b>
                 <p>Will you be attending?</p>
                 <div class="form-check" data-correct="true">
-                    <input class="form-check-input" type="radio" name="eventAttendance1" id="event${index}Attending1">
+                    <input class="form-check-input" type="radio" name="eventAttendance${index}" id="event${index}Attending1" value="${attending[0].correct}">
                     <label class="form-check-label" for="event${index}Attending1">
-                        Yes
+                        ${attending[0].going}
                     </label>
                 </div>
                 
                 <div class="form-check" data-correct="false">
-                    <input class="form-check-input" type="radio" name="eventAttendance1" id="event${index}Attending2">
+                    <input class="form-check-input" type="radio" name="eventAttendance${index}" id="event${index}Attending2" value="${attending[1].correct}">
                     <label class="form-check-label" for="event${index}Attending2">
-                        No
+                        ${attending[1].going}
                     </label>
                 </div>
-
             </div>
         </div>`;
     }
@@ -41,12 +40,48 @@ $(() => {
             return new Date(a.date) - new Date(b.date);
         });
     }
-    //get the events from events.json
-    let json = getEvents("events.json");
-    //create the event cards for each event and append them to the eventList div
-    json.then((resp) => {
-        let cards = resp.map((event, index) => createEventCard(event, index));
-        sortByDate(cards);
-        $("#eventList").append(cards);
+    const load = async() => {
+        //get the events from events.json
+        let json = getEvents("events.json");
+        //create the event cards for each event and append them to the eventList div
+        json.then((resp) => {
+            resp = sortByDate(resp);
+            let cards = resp.map((event, index) => createEventCard(event, index));
+            $("#eventList").append(cards);
+        });
+    }
+    //initial load events
+    load();
+    //add event handler that fires when user selects attending option (radio button)
+    //if user selects "yes" then a bootstrap correct alert is shown (to say the user is going)
+    //if user selects "no" then a bootstrap incorrect alert is shown (to say the user is not going)
+    //these are separate for every card
+    $("#eventList").on("change", "input[type=radio]", (e) => {
+        //attending alert
+        const attendingAlert = $("<div></div>", {
+            class: "alert alert-success",
+            role: "alert",
+            html: "You are attending this event!"
+        });
+        const notAttendingAlert = $("<div></div>", {
+            class: "alert alert-danger",
+            role: "alert",
+            html: "You are not attending this event!"
+        });
+        //get the value of the radio button
+        let going = $(e.target).parent().data("correct");
+        //if the value is true then show the attending alert, otherwise show the not attending alert
+        let alert = going ? attendingAlert : notAttendingAlert;
+        //append the alert to the card
+        //remove any other alerts
+        $(e.target).parent().parent().find(".alert").remove();
+        $(e.target).parent().parent().append(alert);
+    });
+    $(window).on("scroll", function () {
+        // End of the document reached?
+        if($(document).height() - $(this).height() - 100 <= $(this).scrollTop()) {
+            // Load more events
+            load();
+        }
     });
 });
